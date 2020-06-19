67b7166fcd85d73ce9e63fcfcfd9fb87dcdf7d71
Zak Williams <zak.williams2287@gmail.com>
2020-06-19 14:12:02 -0400

    adjusted server env vars

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-added readme
+### Street Eats
+
+## A food delivery app.
+
+- Front-end code on "frontend" branch.
+- Back-end code on "backend" branch.
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("API_HOST", "0.0.0.0"),
+  port: env.int("API_PORT", 1337),
 });
